0680460c5a0e6265aa3c270fe3ad263db603b03c
JCRD05 <josecarlosruizdiaz05@gmail.com>
2025-02-22 10:08:16 -0600

    Update and rename Ejercicios De Arreglos.cs to main.cs

---
+++ main.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ejercicios;
+
+namespace Program
+{
+    class EjerciciosArreglos
+    {
+        static void Main()
+        {
+            Menu menu = new Menu();
+            menu.Show(); // Despliega el menu
+        }
+    }
+}
